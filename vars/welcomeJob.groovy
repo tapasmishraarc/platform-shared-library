7ad0fff5064710a99f32cb38022640f2d235b529
tapasmishraarc <mail.tapasranjanmishra@gmail.com>
2021-04-12 16:47:50 +0530

    Update welcomeJob.groovy

--- vars/welcomeJob.groovy
+++ vars/welcomeJob.groovy
@@ -1,3 +1,5 @@
-def call() {
-   echo 'hi all'
+def call(String name = 'human') {
+    // Any valid steps can be called from this code, just like in other
+    // Scripted Pipeline
+    echo "Hello, ${name}."
 }
