4e846fac6baa950b87afcb72cf71f9bc5ed87279
tapasmishraarc <mail.tapasranjanmishra@gmail.com>
2021-04-13 16:29:33 +0530

    Update welcomeJob.groovy

--- vars/welcomeJob.groovy
+++ vars/welcomeJob.groovy
@@ -1,5 +1,5 @@
 def call(Map config=[:], Closure body) {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
-    echo "Hello, config.type}."
+    echo "Hello ${config.name}"
 }
