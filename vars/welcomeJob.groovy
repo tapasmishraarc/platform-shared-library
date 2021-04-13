03ac1ef4800bd2137fb4d6634882306b7d2fc566
tapasmishraarc <mail.tapasranjanmishra@gmail.com>
2021-04-13 17:02:59 +0530

    Update welcomeJob.groovy

--- vars/welcomeJob.groovy
+++ vars/welcomeJob.groovy
@@ -2,4 +2,11 @@ def call(Map config=[:]) {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
     echo "Hello ${config.name}"
+    
+    node {
+        git url: "https://github.com/tapasmishraarc/sample-nodejs"
+
+        stage("Install") {
+            sh "npm install"
+        }
 }
