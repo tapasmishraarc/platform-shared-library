ec7496f7ad91ffb5af47f6e54000c5db80b584b5
tapasmishraarc <mail.tapasranjanmishra@gmail.com>
2021-04-17 22:44:31 +0530

    Update nodeBuild.groovy

--- vars/nodeBuild.groovy
+++ vars/nodeBuild.groovy
@@ -2,7 +2,7 @@ def call(Map config=[:]) {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
     echo "Hello ${config.name}"
-    tools { nodejs "nodejs"}
+   
     node {
         git url: "https://github.com/tapasmishraarc/sample-nodejs"
         
