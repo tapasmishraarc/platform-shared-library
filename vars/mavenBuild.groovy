222e2d77f61e59374e9f8d95d2847e51ea19c3bc
tapasmishraarc <mail.tapasranjanmishra@gmail.com>
2021-04-19 09:50:53 +0530

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,6 +1,26 @@
-def call(script){
-  //def MAVEN_HOME = script.tool "Maven_HOME"
-  //def JAVA_HOME = script.tool "JAVA_HOME"
-  
- // script.env.path = 
-}
+def call(String script) {
+      echo "Hello Vanshika welcome to MavenBuild shared library"
+   
+       node {
+           stage("Tools initialization") {
+                   sh "mvn --version"
+                   sh "java -version"
+           }
+           stage("Checkout Code") {
+                   git branch: 'master',
+                       url: script.env.GIT_SOURCE_URL
+           }
+	   stage("Cleaning workspace") {
+                   sh "mvn clean"
+           }
+	   stage("Compling") {
+                   sh "mvn compile"
+           }
+           stage("Running Testcase") {
+                   sh "mvn test"
+           }
+           stage("Packing Application") {
+                   sh "mvn package -DskipTests"
+           }
+       }
+   }
