01a1e84ea59d3ecb8b7dcf70a79765829d5cb291
tapasmishraarc <mail.tapasranjanmishra@gmail.com>
2021-04-19 15:06:52 +0530

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -16,9 +16,28 @@ def call(script) {
 	   stage("Compling") {
                    sh "mvn compile"
            }
+	   if(script.env.CODE_QUALITY == 'True'){
+		 stage("Code Quality Analysis") {
+		     echo 'Code Quality Analysis'
+		 }
+	   }
+	   if(script.env.UNIT_TESTING == 'True'){
            stage("Running Testcase") {
                    sh "mvn test"
            }
+	   }
+	   if(script.env.CODE_COVERAGE == 'True')
+        {
+         stage("Code Coverage") {
+             echo 'Code Coverage'
+         }
+        }
+         if(script.env.SECURITY_TESTING == 'True')
+        {
+         stage("Security Testing") {
+             echo 'Security Testing'
+         }
+        }
            stage("Packing Application") {
                    sh "mvn package -DskipTests"
            }
