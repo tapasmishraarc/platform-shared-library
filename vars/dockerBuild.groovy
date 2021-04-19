833fe51765a0c639e1c09c5e2110fa3426d733f9
lgoyal8497 <74949083+lgoyal8497@users.noreply.github.com>
2021-04-19 17:21:14 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,1 +1,21 @@
-
+def call(script) {
+      echo "Hello Vanshika welcome to MavenBuild shared library"
+   
+       node {
+         stage('Docker Build') {
+      agent any
+      steps {
+        sh 'docker build -t shanem/spring-petclinic:latest .'
+      }
+    }
+    stage('Docker Push') {
+      agent any
+      steps {
+        withCredentials([usernamePassword(credentialsId: 'dockerHub', passwordVariable: 'dockerHubPassword', usernameVariable: 'dockerHubUser')]) {
+          sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPassword}"
+          sh 'docker push shanem/spring-petclinic:latest'
+        }
+      }
+    }
+  }
+}
