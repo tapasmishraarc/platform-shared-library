395e212f055f2c7a5893797f7e32e706b0ddae6a
tapasmishraarc <mail.tapasranjanmishra@gmail.com>
2021-04-20 15:17:58 +0530

    Update nodeBuild.groovy

--- vars/nodeBuild.groovy
+++ vars/nodeBuild.groovy
@@ -4,7 +4,7 @@ def call(Map config=[:],script) {
     echo "Hello ${config.name}"
    
     node {
-        git url: script.env.GIT_SOURCE_URL
+       // git url: script.env.GIT_SOURCE_URL
         
         stage("Install") {
             sh "npm install"
