e535441ed3aab4532fb830a14e64303f71aa86e9
Vanshika4222 <67817404+Vanshika4222@users.noreply.github.com>
2021-04-20 15:04:43 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -4,10 +4,7 @@ def call(script) {
    
        node {
 
-             stage("Checkout Code") {
-                   git branch: 'master',
-                       url: script.env.GIT_SOURCE_URL
-           }
+        
              stage('Docker Build') {
                    echo "In docker build stage"
                    sh "docker build -t  ${script.env.DOCKER_REGISTRY}/${script.env.DOCKER_REPO}:latest ."
