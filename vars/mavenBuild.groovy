3b21f05999357ecc6891b12493867840334770e2
tapasmishraarc <mail.tapasranjanmishra@gmail.com>
2021-04-22 21:28:32 +0530

    Update mavenBuild.groovy

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,6 +1,6 @@
 def call(script) {
       echo "Hello Vanshika welcome to MavenBuild shared library"
-  /* podTemplate(yaml """\
+   podTemplate(yaml """\
         apiVersion: v1
         kind: Pod
         metadata:
@@ -18,14 +18,18 @@ def call(script) {
             command:
             - cat
             tty: true
-        """.stripIndent())*/
+        """.stripIndent())
        node {
+	   
 	  
            stage("Tools initialization") {
+		   container('maven')
+	       {
                    sh "mvn --version"
                    sh "java -version"
-           }
-	       
+	       }
+	       }
+	 /*      
          stage("Checkout Code") {
                    git branch: 'master',
                        url: script.env.GIT_SOURCE_URL
@@ -62,6 +66,6 @@ def call(script) {
            stage("Packaging Application") {
                    sh "mvn package -DskipTests"
            }
-	   
+	   */
        }
    }
